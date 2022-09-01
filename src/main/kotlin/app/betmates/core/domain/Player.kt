e37d79c8c158f24d1c56adc990375a6dedd0aafb
Atilla Barros <atilla8huno@gmail.com>
2022-09-01 20:34:37 +0100

    #9 Added Exposed dependency

--- src/main/kotlin/app/betmates/core/domain/Player.kt
+++ src/main/kotlin/app/betmates/core/domain/Player.kt
@@ -4,10 +4,10 @@ class Player(
     val nickName: String,
     private val memberOf: MutableSet<Team> = mutableSetOf()
 ) {
-    fun addToTeam(team: Team): Boolean = memberOf.add(team)
+    fun <T : Team> addToTeam(team: T): Boolean = memberOf.add(team)
         .and(if (this !in team.players()) team.addPlayer(this) else true)
 
-    fun leaveTeam(team: Team) = memberOf.remove(team)
+    fun <T : Team> leaveTeam(team: T) = memberOf.remove(team)
         .and(if (this in team.players()) team.removePlayer(this) else true)
 
     fun memberOf() = memberOf.toSet()
