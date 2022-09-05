23a09b2688775be1a7ee8cb4ca04e3957a31bd98
Átilla Barros <atilla8huno@gmail.com>
2022-09-05 16:37:53 +0100

    #9 Using ktor/hocon application.conf to define DB properties (#18)
    * #9 Using ktor application.conf to define DB properties
    
    * #9 removed user and password from jdbc url

---
+++ src/main/kotlin/app/betmates/core/util/PropertiesUtil.kt
@@ -0,0 +1,22 @@
+package app.betmates.core.util
+
+import com.typesafe.config.ConfigFactory
+import io.ktor.server.config.HoconApplicationConfig
+import io.ktor.server.config.tryGetString
+
+sealed class PropertiesUtil {
+    private val config = HoconApplicationConfig(ConfigFactory.load())
+
+    fun getProperty(key: String): String? = config.tryGetString(key)
+}
+
+object DatabaseProperties : PropertiesUtil() {
+    val username = getProperty("ktor.db.username")
+    val password = getProperty("ktor.db.password")
+    val host = getProperty("ktor.db.host")
+    val port = getProperty("ktor.db.port")
+    val database = getProperty("ktor.db.database")
+    val options = getProperty("ktor.db.options")
+
+    val jdbcUrl = "jdbc:postgresql://$host:$port/$database?options=$options"
+}
---
+++ src/test/kotlin/app/betmates/core/util/PropertiesUtilUTest.kt
@@ -0,0 +1,35 @@
+package app.betmates.core.util
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+
+internal class PropertiesUtilUTest {
+
+    @Test
+    fun `should have defined JDBC URL`() {
+        // given
+        val expectedJdbcUrl = "jdbc:postgresql://" +
+            "${DatabaseProperties.host}:${DatabaseProperties.port}/" +
+            "${DatabaseProperties.database}?" +
+            "options=${DatabaseProperties.options}"
+
+        // when
+        val jdbcUrl = DatabaseProperties.jdbcUrl
+
+        // then
+        assertEquals(expectedJdbcUrl, jdbcUrl)
+    }
+
+    @Test
+    fun `should read DatabaseProperties properties`() {
+        // given DatabaseProperties properties
+        // when access its fields
+        // then should be defined in application.conf
+        assertFalse { DatabaseProperties.username.isNullOrBlank() }
+        assertFalse { DatabaseProperties.host.isNullOrBlank() }
+        assertFalse { DatabaseProperties.port.isNullOrBlank() }
+        assertFalse { DatabaseProperties.database.isNullOrBlank() }
+        assertFalse { DatabaseProperties.options.isNullOrBlank() }
+    }
+}
