109fbb92919cbc8f83fc609f4dd9c16a93f54dac
Atilla Barros <atilla8huno@gmail.com>
2022-09-10 18:30:06 +0100

    Revert "#22 Upgraded Kotlin version (#23)"
    This reverts commit adc4bd47cd0ae1dc4f2e2c143b66ed26e350e333.

--- src/main/kotlin/app/betmates/core/ktor/plugins/Serialization.kt
+++ src/main/kotlin/app/betmates/core/ktor/plugins/Serialization.kt
@@ -1,7 +1,7 @@
 package app.betmates.core.ktor.plugins
 
 import com.fasterxml.jackson.databind.DeserializationFeature
-import io.ktor.serialization.kotlinx.json.jackson.jackson
+import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
