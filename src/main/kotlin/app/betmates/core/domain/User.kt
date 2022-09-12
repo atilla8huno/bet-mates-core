f26f1725b652788c9ca60c0262e2b9b61a8000d1
Átilla Barros <atilla8huno@gmail.com>
2022-09-12 09:41:45 +0100

    #24 User domain to accept password (SHA3-256) (#26)

--- src/main/kotlin/app/betmates/core/domain/User.kt
+++ src/main/kotlin/app/betmates/core/domain/User.kt
@@ -1,15 +1,40 @@
 package app.betmates.core.domain
 
+import java.security.MessageDigest
+
+private val sha3: MessageDigest = MessageDigest.getInstance("SHA3-256")
+
 class User(
     val name: String,
     val email: String,
     val username: String = email.split("@")[0]
 ) {
     private var status: Status = Status.ACTIVE
+    var encryptedPassword: String? = null
+        private set
 
     fun isActive() = status == Status.ACTIVE
 
     fun deactivate() {
         status = Status.INACTIVE
     }
+
+    fun acceptPassword(password: String) {
+        val hashBytes: ByteArray = sha3.digest(
+            password.toByteArray(Charsets.UTF_8)
+        )
+        encryptedPassword = hashBytes.toHex()
+    }
+}
+
+fun ByteArray.toHex(): String {
+    val hexString = StringBuilder(2 * this.size)
+    for (i in this.indices) {
+        val hex = Integer.toHexString(0xff and this[i].toInt())
+        if (hex.length == 1) {
+            hexString.append('0')
+        }
+        hexString.append(hex)
+    }
+    return hexString.toString()
 }
--- src/test/kotlin/app/betmates/core/domain/UserUTest.kt
+++ src/test/kotlin/app/betmates/core/domain/UserUTest.kt
@@ -4,6 +4,7 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 internal class UserUTest {
@@ -41,5 +42,23 @@ internal class UserUTest {
 
         // then
         assertFalse { user.isActive() }
+    }
+
+    @Test
+    fun `should accept password and encrypt it into SHA3-256 hash`() {
+        // given
+        val password = "I51tS3c4r3?"
+        val expectedHash = "2190797d646271ef99ab9077d737ace1aec386586a602da8753a9dcdea2eba2e"
+        val user = User(
+            name = "John Doe",
+            email = "email@mail.com"
+        )
+        assertNull(user.encryptedPassword)
+
+        // when
+        user.acceptPassword(password)
+
+        // then
+        assertEquals(expectedHash, user.encryptedPassword!!)
     }
 }
