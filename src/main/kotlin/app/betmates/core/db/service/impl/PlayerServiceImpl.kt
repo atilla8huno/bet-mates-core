53a11ccc83205772e0d8b131304ba7161c9c24d1
Atilla Barros <atilla8huno@gmail.com>
2022-09-19 22:21:47 +0100

    #27 #9 Removed redundant coroutineScope block

--- src/main/kotlin/app/betmates/core/db/service/impl/PlayerServiceImpl.kt
+++ src/main/kotlin/app/betmates/core/db/service/impl/PlayerServiceImpl.kt
@@ -6,7 +6,6 @@ import app.betmates.core.db.entity.UserRepository
 import app.betmates.core.db.service.PlayerService
 import app.betmates.core.db.service.UserService
 import app.betmates.core.domain.Player
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.map
@@ -18,47 +17,37 @@ class PlayerServiceImpl(
     private val database: Database = DatabaseConnection.database
 ) : PlayerService {
 
-    override suspend fun save(domain: Player): Player = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            val playerId = PlayerRepository.new {
+    override suspend fun save(domain: Player): Player = newSuspendedTransaction(db = database) {
+        val playerId = PlayerRepository.new {
+            nickName = domain.nickName
+            user = UserRepository.findById(domain.user.id!!)!!
+        }.id.value
+
+        domain.apply { id = playerId }
+    }
+
+    override suspend fun update(domain: Player): Player = newSuspendedTransaction(db = database) {
+        PlayerRepository.findById(domain.id!!)!!
+            .apply {
                 nickName = domain.nickName
                 user = UserRepository.findById(domain.user.id!!)!!
-            }.id.value
-
-            domain.apply { id = playerId }
-        }
-    }
-
-    override suspend fun update(domain: Player): Player = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            PlayerRepository.findById(domain.id!!)!!
-                .apply {
-                    nickName = domain.nickName
-                    user = UserRepository.findById(domain.user.id!!)!!
-                }.let {
-                    mapToDomain(it)
-                }
-        }
-    }
-
-    override suspend fun findById(id: Long): Player? = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            PlayerRepository.findById(id)?.let {
+            }.let {
                 mapToDomain(it)
             }
+    }
+
+    override suspend fun findById(id: Long): Player? = newSuspendedTransaction(db = database) {
+        PlayerRepository.findById(id)?.let {
+            mapToDomain(it)
         }
     }
 
-    override suspend fun findAll(): Flow<Player> = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            PlayerRepository.all().asFlow().map { mapToDomain(it) }
-        }
+    override suspend fun findAll(): Flow<Player> = newSuspendedTransaction(db = database) {
+        PlayerRepository.all().asFlow().map { mapToDomain(it) }
     }
 
-    override suspend fun delete(domain: Player): Unit = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            PlayerRepository.findById(domain.id!!)?.delete()
-        }
+    override suspend fun delete(domain: Player): Unit = newSuspendedTransaction(db = database) {
+        PlayerRepository.findById(domain.id!!)?.delete()
     }
 
     override fun mapToDomain(entity: PlayerRepository): Player {
--- src/main/kotlin/app/betmates/core/db/service/impl/UserServiceImpl.kt
+++ src/main/kotlin/app/betmates/core/db/service/impl/UserServiceImpl.kt
@@ -6,7 +6,6 @@ import app.betmates.core.db.entity.UserRepository
 import app.betmates.core.db.service.UserService
 import app.betmates.core.domain.Status
 import app.betmates.core.domain.User
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.firstOrNull
@@ -19,75 +18,61 @@ class UserServiceImpl(
     private val database: Database = DatabaseConnection.database
 ) : UserService {
 
-    override suspend fun save(domain: User): User = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            val userId = UserRepository.new {
+    override suspend fun save(domain: User): User = newSuspendedTransaction(db = database) {
+        val userId = UserRepository.new {
+            name = domain.name
+            email = domain.email
+            username = domain.username
+            status = if (domain.isActive()) Status.ACTIVE.name else Status.INACTIVE.name
+            password = domain.encryptedPassword
+        }.id.value
+
+        domain.apply { id = userId }
+    }
+
+    override suspend fun update(domain: User): User = newSuspendedTransaction(db = database) {
+        UserRepository.findById(domain.id!!)!!
+            .apply {
                 name = domain.name
                 email = domain.email
                 username = domain.username
                 status = if (domain.isActive()) Status.ACTIVE.name else Status.INACTIVE.name
-                password = domain.encryptedPassword
-            }.id.value
-
-            domain.apply { id = userId }
-        }
-    }
-
-    override suspend fun update(domain: User): User = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            UserRepository.findById(domain.id!!)!!
-                .apply {
-                    name = domain.name
-                    email = domain.email
-                    username = domain.username
-                    status = if (domain.isActive()) Status.ACTIVE.name else Status.INACTIVE.name
-                }.let {
-                    mapToDomain(it)
-                }
-        }
-    }
-
-    override suspend fun delete(domain: User): Unit = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            UserRepository.findById(domain.id!!)?.delete()
-        }
-    }
-
-    override suspend fun findById(id: Long): User? = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            UserRepository.findById(id)?.let {
+            }.let {
                 mapToDomain(it)
             }
+    }
+
+    override suspend fun delete(domain: User): Unit = newSuspendedTransaction(db = database) {
+        UserRepository.findById(domain.id!!)?.delete()
+    }
+
+    override suspend fun findById(id: Long): User? = newSuspendedTransaction(db = database) {
+        UserRepository.findById(id)?.let {
+            mapToDomain(it)
         }
     }
 
-    override suspend fun findAll(): Flow<User> = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            UserRepository.all().asFlow().map { mapToDomain(it) }
-        }
+    override suspend fun findAll(): Flow<User> = newSuspendedTransaction(db = database) {
+        UserRepository.all().asFlow().map { mapToDomain(it) }
     }
 
-    override suspend fun findByEmail(email: String): User? = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            UserRepository.find { UserEntity.email eq email }.firstOrNull()?.let {
-                mapToDomain(it)
-            }
+    override suspend fun findByEmail(email: String): User? = newSuspendedTransaction(db = database) {
+        UserRepository.find { UserEntity.email eq email }.firstOrNull()?.let {
+            mapToDomain(it)
         }
     }
 
     override suspend fun updatePassword(
         user: User,
         newPassword: String
-    ): User? = coroutineScope {
-        newSuspendedTransaction(db = database) {
-            UserRepository.find {
-                (UserEntity.id eq user.id!!) and (UserEntity.password eq user.encryptedPassword!!)
-            }.firstOrNull()?.let {
-                user.acceptPassword(newPassword)
-                it.password = user.encryptedPassword
+    ): User? = newSuspendedTransaction(db = database) {
+        UserRepository.find {
+            (UserEntity.id eq user.id!!) and (UserEntity.password eq user.encryptedPassword!!)
+        }.firstOrNull()?.let {
+            user.acceptPassword(newPassword)
+            it.password = user.encryptedPassword
 
-                user
-            }
+            user
         }
     }
 
